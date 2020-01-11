0d5a72d1d6bc018442f54f92b54d83fcafb1f589
dam <kokocik1213@gmail.com>
2020-01-11 21:09:47 +0100

    Code refactor

--- src/main/java/controllers/client/view/panels/panels_parts/PanelState.java
+++
@@ -1,9 +0,0 @@
-package controllers.client.view.panels.panels_parts;
-
-import javax.swing.*;
-
-public interface PanelState {
-   PanelState next(int value);
-   void refresh(int x, int y, int playerId);
-   JPanel getPanel();
-}
---
+++ window_app/src/main/java/MainPlayer.java
@@ -0,0 +1,8 @@
+import view.PlayerPanel;
+
+public class MainPlayer {
+
+	public static void main(String args[]) {
+		PlayerPanel client = new PlayerPanel();
+	}
+}
